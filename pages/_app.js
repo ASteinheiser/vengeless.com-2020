dbb2318b0809bb77c8a0022154fdebfea5c38108
Andrew Steinheiser <asteinheiser@magicleap.com>
2020-10-17 13:08:55 -0700

    basic theming

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import 'fontsource-roboto'
 
 import '../styles/globals.css'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#7129bf'
+    },
+    secondary: {
+      main: '#7c7c7c'
+    }
+  }
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
